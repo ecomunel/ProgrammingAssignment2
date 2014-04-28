25ee58d4e08c7abd35be51f3f5b240fea0843554
ecomunel <ecomunel@users.noreply.github.com>
2014-04-27 17:08:55 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# <code r>
+# =============================================================================
+#
+# Creates a special "matrix" object that can cache its inverse!
+#
 makeCacheMatrix <- function(x = matrix()) {
-
+          mat  <- NULL
+          set  <- function(y) {
+                    x <<- y
+                    mat <<- NULL
+          }
+          get    <- function() x
+          setInv <- function(inv) mat <<- inv
+          getInv <- function() mat
+          list(set = set, get = get,
+                setInv = setInv,
+                getInv = getInv)
 }
-
-
-## Write a short comment describing this function
-
+#
+# =============================================================================
+#
+# Computes the inverse of the special "matrix" returned by makeCacheMatrix()
+# If the inverse has already been calculated, retrieve the inverse from the cache.
+#
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          mat <- x$getInv()
+          if(!is.null(mat)) {
+                    message("\nUsing inverse matrix from cache!\n\n")
+                    return(mat)
+          }
+          matrix <- x$get()
+          mat    <- solve(matrix, ...)
+          x$setInv(mat)
+          mat
 }
+#
+# =============================================================================
+#
+# Testing...
+#
+mat  <- matrix(c(1,6,1,7,7,5,2,4,9),3,3)
+cMat <- makeCacheMatrix(mat)
+#
+ls.str(cMat)
+cMat$get()
+cMat$getInv()
+#
+cacheSolve(cMat)
+cMat$getInv()
+cacheSolve(cMat)
+# </code>
